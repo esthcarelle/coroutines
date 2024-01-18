4c8b5ad221446f793819890aef7bfc82480c03bb
Esther Carrelle <esthcarelle@gmail.com>
2024-01-18 18:19:28 +0200

    return viewModelScope

--- start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
+++ start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
@@ -22,6 +22,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.android.kotlincoroutines.util.BACKGROUND
 import com.example.android.kotlincoroutines.util.singleArgViewModelFactory
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -142,6 +143,9 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
         }
     }
     private fun launchDataLoad(block: suspend () -> Unit): Job {
+        return viewModelScope.launch {
 
-    }
+        }
+
+        }
 }
